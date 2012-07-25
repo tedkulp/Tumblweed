2b945c2ca1aaadf65d597c5a5b4948a66b7794a9
Ted Kulp <ted@tedkulp.com>
2012-07-25 06:26:03 -0400

    Make the photo cells work

--- app/cells/photo_cell.rb
+++ app/cells/photo_cell.rb
@@ -1,16 +1,31 @@
 class PhotoCell < PostCell
 
   def height
-    75
+    67
   end
 
   def setupSubviewArray
-    @img = UIImageView.alloc.initWithFrame([[0, 0], [75, 75]])
-    [@img]
+    @img = UIImageView.alloc.initWithFrame([[320 - 64 - 1, 1], [64, 64]])
+    @blog_name = UILabel.alloc.initWithFrame([[5, 5], [320 - 64 - 1, 20]])
+    @caption = UILabel.alloc.initWithFrame(CGRectZero)
+    @caption.setLineBreakMode(UILineBreakModeWordWrap)
+    @caption.setMinimumFontSize(12)
+    @caption.setFont(UIFont.systemFontOfSize(12))
+    [@img, @blog_name, @caption]
   end
 
   def updateViewsFromPost(post)
     @img.image = UIImage.imageWithData(NSData.dataWithContentsOfURL(NSURL.URLWithString(post.photos[0]['alt_sizes'].last['url'])))
+    @blog_name.text = post.blog_name
+
+    constraint = CGSizeMake(320 - (5 * 2) - 65, 20000.0);
+    size = post.caption.sizeWithFont(UIFont.systemFontOfSize(12), constrainedToSize:constraint, lineBreakMode:UILineBreakModeWordWrap)
+
+    @caption.text = post.caption.gsub(/<\/?[^>]*>/, '')
+    @caption.setFrame(CGRectMake(5, 25, 320 - (5 * 2) - 65, [size.height, 67 - 25].max))
+    # @caption.layer.setBorderWidth(2.0)
+    @caption.numberOfLines = 0
+    @caption.sizeToFit
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post
 
-  attr_accessor :id, :post_url, :type, :timestamp, :date, :tags, :cell_view
+  attr_accessor :id, :post_url, :type, :timestamp, :date, :tags, :cell_view, :blog_name
 
   def initialize(attributes = {})
     attributes.each { |key, value|
